da8ce13e43e71957871ba0c2b1201c9bf6e2b442
andrew-waring <andrew@digital-guerrilla.scot>
2025-07-06 12:45:55 +0000

    fixes

--- run.py
+++ run.py
@@ -12,4 +12,4 @@ app = create_app()
 if __name__ == '__main__':
     # For local development only
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
-    app.run(debug=debug_mode, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(debug=debug_mode, host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
